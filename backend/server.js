6c4e2a9a1fe030e2f5ae6e4e9bf5f2c85a19c16e
ethangent <ethgent@gmail.com>
2019-03-19 13:42:58 -0400

    added some routes

--- backend/server.js
+++ backend/server.js
@@ -6,18 +6,18 @@ const db = require("./model/gif")
 const axios = require("axios");
 
 const API_PORT = process.env.PORT || 3001;
-const app = express();
+const router = express();
 
-app.use(express.urlencoded( { extended: false}));
-app.use(express.json());
-app.use(logger("dev"));
+router.use(express.urlencoded( { extended: false}));
+router.use(express.json());
+router.use(logger("dev"));
 
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/gifs";
 mongoose.connect(MONGODB_URI, {useNewUrlParse: true });
 
 
-app.get("/", (req, res) => {
+router.get("/srch", (req, res) => {
     const giphy = {
         baseURL: "https://api.giphy.com/v1/gifs/",
         key: "&api_key=r5zJ6IQVZrTVcmo4vNJxx0r3FnDMqrPW",
@@ -52,9 +52,16 @@ app.get("/", (req, res) => {
             .catch(error => console.log(error));
 });
 
-// app.use("/api", app);
+router.get("/getData", (req, res) => {
+    db.find((err, data) => {
+      if (err) return res.json({ success: false, error: err });
+      return res.json({ success: true, data: data });
+    });
+  });
+
+router.use("/api", router);
 
 // launch our backend into a port
-app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
+router.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
 
 
